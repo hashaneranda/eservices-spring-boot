c11447c5512893ed486c56f0dccc25e331317efa
hashaneranda <hashaneranda@gmail.com>
2022-04-20 22:32:04 +0530

    Gateway service added

---
+++ api-gateway/src/main/java/com/hashan/nagp/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.hashan.nagp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableHystrix
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
